6a7376502e4097375375894c76cb8987451400c6
Washington Kibichii <38534263+washington-kibichii@users.noreply.github.com>
2019-04-03 15:41:35 +0300

    Update settings.js

--- packages/subscription-widget/settings.js
+++ packages/subscription-widget/settings.js
@@ -1,12 +1,12 @@
 // Change the url to the domain of your app
-exports.url = 'http://localhost:3090';
+exports.url = 'https://widget-test-radio-africa.herokuapp.com/';
 
-exports.senderEmail = 'sender@example.com';
-exports.senderName = 'Sender Name';
+exports.senderEmail = 'info@the-star.co.ke';
+exports.senderName = 'The Star - Kenya';
 
 // set 'exports.listId = null' to add contact to all contacts, but no specific list
 // or a string with the listId to add to a specific list
-exports.listId = null;
+exports.listId = 7502868;
 
 // set 'exports.templateId = null' to opt out of using a template
 // or a string with the templateId to use a template
@@ -14,4 +14,4 @@ exports.templateId = null;
 
 // receive an email when a new signup is confirmed
 exports.sendNotification = true;
-exports.notificationEmail = 'admin@example.com';
+exports.notificationEmail = 'kibichii33@gmail.com';
